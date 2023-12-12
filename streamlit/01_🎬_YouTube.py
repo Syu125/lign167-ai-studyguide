59f310510a5d5cb7b2bf12fa7ad065baf78ebf78
Syu125 <yusophia2011@gmail.com>
2023-12-12 10:27:28 -0800

    add markdown print

--- streamlit/01_🎬_YouTube.py
+++ streamlit/01_🎬_YouTube.py
@@ -286,6 +286,7 @@ def display_topics_and_sections_ordered():
                         with st.expander(f"Upload Transcript"):
                             uploaded_file = st.file_uploader(f"Upload here:", type="txt", key=f"transcript_{index}")
                             transcript_path = os.path.join(transcript_storage_path, row['Topic'] + ".txt")
+                            st.markdown(transcript_path)
                             if uploaded_file is not None:
                                 file_content = uploaded_file.getvalue().decode("utf-8")
                                 try:
